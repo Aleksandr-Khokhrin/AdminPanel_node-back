e3129e399613722c350ed98b15784fd270a51b0f
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-08-21 18:16:45 +0500

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const authRouter = require('./authRouter')
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
